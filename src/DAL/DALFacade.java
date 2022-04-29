e7b6272a0f93869455714f5217ecf7c626e45671
samkaxe <90753108+Samkaxe@users.noreply.github.com>
2022-04-29 11:35:29 +0200

    Dal

--- src/DAL/DALFacade.java
+++ src/DAL/DALFacade.java
@@ -1,10 +1,22 @@
 package DAL;
 
 import BE.User;
+import DAL.util.DalException;
+
+import java.util.List;
 
 public interface DALFacade {
 
 
-    User verifyUsers(String useremail, String password);
+    User verifyUsers(String useremail, String password) throws DalException;
 
+    List<User> getAllUsers() throws DalException;
+
+    void updateuser(User user , String username , String email , String userType) throws DalException;
+
+    void deleteuser(User user) throws DalException;
+
+    User addUser(String username , String password , String email , String usertype) throws DalException;
+
+    List<User> searchForUser (String query) throws DalException;
 }
