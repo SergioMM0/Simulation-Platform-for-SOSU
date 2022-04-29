94f2bd199ebea60c49d2b7f4a70aa5396e0b4510
Sergio M <serg0971@easv365.dk>
2022-04-29 11:12:45 +0200

    BE

--- src/BE/User.java
+++ src/BE/User.java
@@ -1,14 +1,24 @@
 package BE;
 
 public class User {
+    private int id;
     private String name;
     private String email;
     private String userType;
 
-    public User(String name, String email, String userType) {
+    public User(int id,String name, String email, String userType) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.userType = userType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getUserType() {
