1e819a6f414acf2a8e34e073425e3fc06264a372
samkaxe <90753108+Samkaxe@users.noreply.github.com>
2022-05-13 11:13:43 +0200

    update patient

--- src/DAL/Manager1/DAOPatient.java
+++ src/DAL/Manager1/DAOPatient.java
@@ -75,13 +75,12 @@ public class DAOPatient {
         }
     }
 
-/*
+
     public void updatepatient(Patient patient) throws DalException {
 
         try (Connection con = dataAccess.getConnection()){
             String sql = "Update Patient set first_name = ? , last_name = ? , dateoBirth = ? , gender = ? " +
-                    ", weight = ? , height = ? , cpr = ? , phone_number = ? , blood_type = ? , exercise = ?  " +
-                    ", diet = ? ,alcohol = ?,tobacco = ? ,observations = ? where id = ? ";
+                    ", weight = ? , height = ? , cpr = ? , phone_number = ? , observations = ? where id = ? ";
             PreparedStatement prs = con.prepareStatement(sql);
             prs.setString(1 , patient.getFirst_name());
             prs.setString(2 , patient.getLast_name());
@@ -91,20 +90,13 @@ public class DAOPatient {
             prs.setString(6,patient.getHeight());
             prs.setString(7 ,patient.getCpr());
             prs.setString(8 , patient.getPhoneNumber());
-            prs.setString(9,patient.getBloodType());
-            prs.setString(10,patient.getExercise());
-            prs.setString(11, patient.getDiet());
-            prs.setString(12,patient.getAlcohol());
-            prs.setString(13,patient.getTobacco());
-            prs.setString(14,patient.getObservations());
-            prs.setInt(15, patient.getId());
+            prs.setString(9,patient.getObservations());
+            prs.setInt(10, patient.getId());
             prs.executeUpdate();
         } catch (SQLException e) {
             throw new DalException("Connection Lost" , e);
         }
     }
-
- */
 
 
     public void deletePatient(Patient patient) throws DalException {
